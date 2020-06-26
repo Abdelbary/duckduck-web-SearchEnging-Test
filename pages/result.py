6a119107b36b3dfd450423a2b3ba832bd2d2d819
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2020-06-26 19:01:40 +0200

    adding parllel tests

--- pages/result.py
+++ pages/result.py
@@ -1,28 +1,34 @@
 """
-This module contains DuckDuckGoTesulyPage,
+This module contains DuckDuckGoResultPage,
 the page object for the DuckDuckGo search result page.
 """
+
 from selenium.webdriver.common.by import By
 
 
-class DuckDucGoResultPage:
-    
-    SEARCH_RESULT_LINKS = (By.CSS_SELECTOR,'a.result__a')
-    SEARCH_INPUT = (By.ID,'search_form_input')
+class DuckDuckGoResultPage:
 
-    def __init__(self,brower):
-        self.browser = brower
+    # Locators
+
+    RESULT_LINKS = (By.CSS_SELECTOR, 'a.result__a')
+    SEARCH_INPUT = (By.ID, 'search_form_input')
+
+    # Initializer
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    # Interaction Methods
 
     def result_link_titles(self):
-        links = self.browser.find_elements(*self.SEARCH_RESULT_LINKS)
+        links = self.browser.find_elements(*self.RESULT_LINKS)
         titles = [link.text for link in links]
         return titles
 
     def search_input_value(self):
-        searchInput = self.browser.find_element(*self.SEARCH_INPUT)
-        value       = searchInput.get_attribute('value')#get the get_attribute for input elements not the text function
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        value = search_input.get_attribute('value')
         return value
-
 
     def title(self):
         return self.browser.title
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,29 +1,29 @@
-from pages.result import DuckDucGoResultPage
+from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage 
+import pytest
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize('phrase',['panda','python','duck','ahmed','you','too'])
+def test_basic_duckduckgo_search(browser,phrase):
     
     searchPage       = DuckDuckGoSearchPage(browser)
-    serachResultPage = DuckDucGoResultPage(browser)
-    PHASE  = "duck"
+    serachResultPage = DuckDuckGoResultPage(browser)
     
     # Given the DuckDuckGo home page is displayed
     searchPage.load()
 
 
     # When the user searches for "panda"
-    searchPage.search(PHASE)
+    searchPage.search(phrase)
 
-
-    # Then the search result title contains "panda"
-    assert PHASE in serachResultPage.title()
-
-    # And the search result is "panda"
-    assert PHASE == serachResultPage.search_input_value()
+    # Then the search result is "panda"
+    assert phrase == serachResultPage.search_input_value()
 
     browser.get_screenshot_as_png()
     # And the search result links pertain to "panda"
     for title in serachResultPage.result_link_titles():
-        assert PHASE.lower() in title.lower()
-    # TODO remove this exception when done
-    #raise Exception("Incomplete Test")
+        assert phrase.lower() in title.lower()
+
+    
+    # And the search result title contains "panda"
+    assert phrase in serachResultPage.title()
+   
